2e47653cec6d05686b7141936adcc4595f157156
nam256 <125194650+nam256@users.noreply.github.com>
2023-04-05 17:13:46 +0700

    Add files via upload

---
+++ CTDL&GT_Quick_Sort_1.cpp
@@ -0,0 +1,37 @@
+// Cài đặt 
+// https://www.sortvisualizer.com/quicksort/
+
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = partition(arr, low, high);
+
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
+    }
+}
